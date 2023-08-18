23a9e6d19e4f05b4a057988ca060ff5de0edcfc4
Joniffer Mandac <101397206+joniffer10@users.noreply.github.com>
2023-08-19 04:01:45 +0800

    Add files via upload

---
+++ terms.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE-edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" type="image/x-icon" href="img/iconn.ico">
+	<title>Dental Clinic</title>
+    <link rel="stylesheet" href="termpolicy.css">
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<!--==================== NAVBAR ====================-->
+<?php include "navbar.php" ?>
+<!--==================== BANNER ====================-->
+    <section class="banner">
+        <div class="banner_content"> 
+        <h1> Guidelines for a seamless web-app experience</h1>
+        </div>
+    </section>
+
+<!--==================== TERMS OF USE ====================-->
+    <section class="kontint container">
+    <h2 class="section__title">Term of Use</h2>
+        <p> Welcome to Dental Clinic's Web app!
+            <br><br>
+            Please <b>read</b> these terms carefully. By accessing and using this web app, you agree to comply with these Terms of Use describe herein.
+            If you do not agree with any part of these terms, please refrain from using the web app.
+        </p>
+        <h3> 1. User Responsibilities </h3>
+        <p> By accessing and using the Dental Clinic's Web app, you agree to adhere to these Terms of Use. To use the web app, you must be atleast 18 years old or have parental consent.
+            It is your responsibility to ensure the accuracy and completeness of the information you provide. Usage of the web app for illegal or unauthorized purposes is strictly prohibited.
+            Any attempt to interfere with the proper functioning or security features of the web app is also prohibited.
+        </p>
+        <h3> 2. Intellectual Property </h3>
+        <p> All content, designs, and intellectual property found on this web app are the sole property of Dental Clinic.
+            Without explicit permission, you are not authorized to reproduce, modify, or distribute any of the content.
+        </p>
+        <h3> 3. Privacy and Security </h3>
+        <p> Your privacy is of utmost importance to us. We collect and use personal information in accordance with our Privacy Policy.
+            which outlines how we handle your data to provide services effectively and securely.
+        </p>
+        <h3> 4. Disclaimer of Liability </h3>
+        <p> While we strive to provide accurate and reliable information, the Dental Clinic does not guarantee this completeness or accuracy of the content on this web app.
+            Therefore, any reliance you place on such information is at your own risk. We shall not be liable for any direct, indirect, or consequential damages resulting from the use of this web app.
+        </p>
+        <h3> 5. Third-Party Links </h3>
+        <p> This web app may contain links to third-party websites. Dental Clinic holds no responsibility for the content or actions of these external sites.
+        </p>
+        <h3> 6. Changes to the Terms of Use </h3>
+        <p> Dental Clinic reserves the right to update or modify these Terms of Use at any time without prior notice. It is your responsibility to review the terms periodically to stay informed about any changes.
+        </p>
+        <h3> 7. Termination</h3>
+        <p> Dental  Clinic reserves the right to terminate your access to the web app if you violate these Terms of Use or engage in any harmful activities that may compromise the web app's integrity or the experience of other users.
+        </p>
+        <br>
+        <p> By accessing and using our web app, you acknowledge that you have read, understood, and agree to abide by these Terms of Use.
+            If you do not agree with any part of these documents, we kindly request that you refrain from using the Dental Clinic web app.
+        </p>
+    </section>
+	<footer>
+		<?php include "footer.php" ?>
+	</footer>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
